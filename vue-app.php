cd0b8c329d88a24e82e7a970fe8aa3df00458c60
Tardisdoc12 <jean.anquetil@livecampus.tech>
2025-08-21 11:54:17 +0200

    on met le plugin complet

--- vue-app.php
+++ vue-app.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Vue App
- * Description: Intègre une application Vue dans WordPress via des shortcodes [login], [calendar], [events].
+ * Description: Intègre une application Vue dans WordPress via des shortcodes, middleware et des ajouts pour la base de donnée
  * Version: 1.0
  * Author: Toi
  */
@@ -39,3 +39,58 @@ $shortcodes = ['login', 'calendar', 'events', 'connexion'];
 foreach ($shortcodes as $sc) {
     add_shortcode($sc, 'vue_shortcode');
 }
+
+//-----------------------------------------------------------------------------------
+// Middelware
+
+add_action('template_redirect', 'redirect_if_logged_in_jwt');
+
+function redirect_if_logged_in_jwt() {
+    // Vérifie si on est sur la page de connexion (/home/)
+    if (is_page('home')) {
+        $jwt = isset($_COOKIE['mps_moto']) ? $_COOKIE['mps_moto'] : null;
+
+        if ($jwt) {
+            // Si déjà connecté avec un token valide, on redirige
+            wp_redirect(home_url('/test/')); 
+            exit;
+        }
+    }
+}
+
+//-----------------------------------------------------------------------------------
+
+add_action('rest_api_init', function() {
+    $custom_fields = [
+        'firstName',
+        'lastName',
+        'adherentNumber',
+        'telephone',
+        'moto',
+    ];
+
+    foreach ($custom_fields as $field) {
+        register_rest_field('user', $field, [
+            'get_callback' => function($user) use ($field) {
+                return get_user_meta($user['id'], $field, true);
+            },
+            'update_callback' => function($value, $user) use ($field) {
+                update_user_meta($user->ID, $field, sanitize_text_field($value));
+            },
+        ]);
+    }
+
+    register_rest_field('user', 'email', [
+        'get_callback' => function($user) {
+            if (get_current_user_id() === $user['id']) {
+                return get_userdata($user['id'])->user_email;
+            }
+            return null;
+        },
+        'update_callback' => null,
+        'schema' => [
+            'description' => __('User email'),
+            'type' => 'string'
+        ],
+    ]);
+});
