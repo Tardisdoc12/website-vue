761f88c82c18ff6ea30433e0aae1553d3565dce6
Tardisdoc12 <jean.anquetil@livecampus.tech>
2025-09-06 23:07:30 +0200

    on change deux couleurs

--- src/javascript/constants.js
+++ src/javascript/constants.js
@@ -13,10 +13,10 @@ export default {
         if (categorie === seance) {
             return ["rgba(109, 159, 175, 1)","rgba(109, 159, 175,0.2)"]
         }
-        if (categorie === stage) {
+        if (categorie === balade) {
             return ["rgba(37, 158, 77, 1)","rgba(37, 158, 77, 0.2)"]
         }
-        if (categorie === balade) {
+        if (categorie === stage) {
             return ["rgba(164, 46, 194, 1)","rgba(164, 46, 194, 0.2)"]
         }
     },
