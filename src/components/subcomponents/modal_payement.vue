c86826d3a696dc4e080e78c3bbb34330ae87069a
Tardisdoc12 <jean.anquetil@livecampus.tech>
2025-09-23 21:13:01 +0200

    correction d'orthographe

--- src/components/subcomponents/modal_payement.vue
+++ src/components/subcomponents/modal_payement.vue
@@ -4,7 +4,7 @@
         :isCancel="isOpen"
         @changeBool="Cancel"
     >
-        <p style="margin-bottom: 10px; margin-top: 10px;">{{  "Votre inscription ne sera validé qu'après payement" }}</p>
+        <p style="margin-bottom: 10px; margin-top: 10px;">{{  "Votre inscription ne sera validée qu'après paiement" }}</p>
         <iframe src="https://www.helloasso.com/associations/mps-moto/evenements/inscription-seance/widget" style="width: 80vw; height: 80vh; border: none;">
         </iframe>
     </Modal>
