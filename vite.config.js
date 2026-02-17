99e98779372da93c0b2651fa9e587031a90ff644
Tardisdoc12 <jean.anquetil12@gmail.com>
2026-02-17 13:32:35 +0100

    on supprime des lignes inutiles

--- vite.config.js
+++ vite.config.js
@@ -5,18 +5,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from "@tailwindcss/vite"
 
-// Récupère l'argument --entry
-const customEntry = process.env.ENTRY
-
-// Définit une map des entrées possibles
-const entries = {
-  app: path.resolve(__dirname, 'src/main.js'),
-}
-
-// Si un ENTRY est passé → build seulement celle-là
-// Sinon → build la première par défaut
-const input = customEntry ? { [customEntry]: entries[customEntry] } : { login: entries.login }
-
 export default defineConfig({
   plugins: [
     vue(),
