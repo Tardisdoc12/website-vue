54bd5a43e47e1660d09cc0f9b6916c29550fbe9b
Tardisdoc12 <jean.anquetil12@gmail.com>
2026-02-20 19:59:56 +0100

    on met les bon shortcodes

--- wordpress_code/includes/functions.php
+++ wordpress_code/includes/functions.php
@@ -46,14 +46,13 @@ function monplugin_verify_csrf(WP_REST_Request $request) {
 function get_vue_shortcodes() {
     return [
         // shortcode => Files vue
-        'login'            => 'sign_in',
         'calendar'         => 'calendar',
         'compte'           => 'global_compte',
-        'adherent'         => 'adherent',
-        'connexion'        => 'connexion',
-        'espace_profil'    => 'espace_profil',
-        'gestions_comptes' => 'gestions_comptes',
+        'form_adhesion'    => 'adherent',
+        'test'             => 'espace_profil',
+        'account'          => 'gestions_comptes',
         'event-page'       => 'event_page_proper',
+        'reinitialisation' => 'reset_password_page',
     ];
 }
 
