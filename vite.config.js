f80a9ea7ddf67eee3c1f0d67f4da35204b74abe8
JeanA12 <jean.anquetil12@gmail.com>
2026-06-15 20:10:54 +0200

    maj de vite 8 permettant de corriger des failles importantes

--- vite.config.js
+++ vite.config.js
@@ -6,18 +6,24 @@ import vue from '@vitejs/plugin-vue'
 import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig({
+  css: {
+    transformer: 'postcss',
+  },
   plugins: [
     vue(),
     tailwindcss(),
   ],
   define: {
     'process.env': {},
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV ?? 'production'),
     global: 'globalThis'
   },
   build: {
     manifest: true,
     outDir: 'build/dist',
     emptyOutDir: true,
+    cssMinify: 'esbuild',
+    minify: 'esbuild',
     lib: {
       entry: path.resolve(__dirname, 'src/main.js'),
       name: 'VueLoginApp',
