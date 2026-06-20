59669e27f03d5b6e15e78edbac7f5d58b4719bbe
JeanA12 <jean.anquetil12@gmail.com>
2026-06-20 11:47:15 +0200

    suppresssion d'un espace

--- wordpress_code/includes/rest-subscribe.php
+++ wordpress_code/includes/rest-subscribe.php
@@ -82,7 +82,7 @@ function monplugin_create_subscribe(WP_REST_Request $request) {
     $table_events   = $wpdb->prefix . "events";
     $table_inscrits = $wpdb->prefix . "inscrits";
     $table_users    = $wpdb->prefix . "users_inscrits";
-    $event_id = $request->get_param('event_id'); 
+    $event_id = $request->get_param('event_id');
     $user_d = $request->get_param('user');
     $roles    = isset($user_d['roles']) && is_array($user_d['roles']) ? array_map('sanitize_text_field', $user_d['roles']) : ['non_adherent'];
     $isAddAdmin = boolval($request->get_param('isAddAdmin'));
