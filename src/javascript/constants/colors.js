334db51c9fd7f66e5d8e413e9441d2adb61d9f3e
jeanA12 <jean.anquetil12@gmail.com>
2026-09-13 12:57:52 +0200

    on supprime le code inutile du au paramétrage

--- src/javascript/constants/colors.js
+++ src/javascript/constants/colors.js
@@ -10,11 +10,4 @@ export const Couleurs = {
     vert: '#22c55e',
     dark_vert: '#19773c',
     black: "#000000",
-    // event colors
-    seance_main: "rgba(109, 159, 175, 1)",
-    seance_second: "rgba(109, 159, 175, 0.2)",
-    ballade_main: "rgba(37, 158, 77, 1)",
-    ballade_second: "rgba(37, 158, 77, 0.2)",
-    stage_main: "rgba(164, 46, 194, 1)",
-    stage_second: "rgba(164, 46, 194, 0.2)",
 }
