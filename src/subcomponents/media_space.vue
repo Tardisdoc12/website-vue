1f3a92254123bfa6a29a95e23b91ec0b8773ca8a
jeanA12 <jean.anquetil12@gmail.com>
2026-09-13 05:24:56 +0200

    on met en place le bouton pour partager les média

--- src/subcomponents/media_space.vue
+++ src/subcomponents/media_space.vue
@@ -1,5 +1,6 @@
 <template>
-    <p>Espace média en cours de construction</p>
+    <p>Partager vos médias:</p>
+    <a href="https://kdrive.infomaniak.com/app/collaborate/3180869/6d6e6a11-cabe-432e-a774-d3e3122a6378" target="_blank">Partager</a>
 </template>
 
 <script>
